52f968fd3bff9453addb763e58c2967cff982e8f
anano303 <124179773+anano303@users.noreply.github.com>
2024-11-18 15:04:29 +0400

    .

--- server/api/routes/expenses/expenses.js
+++ server/api/routes/expenses/expenses.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const { authenticateToken, authorizeAdmin } = require("../../middleware/auth");
-const { readExpenses, writeExpenses } = require("../../../../fileHandler");
+
 const {
   getExpenses,
   addExpenses,
@@ -12,7 +12,7 @@ const expenseRoutes = Router();
 
 expenseRoutes.get("/", getExpenses);
 expenseRoutes.post("/", authenticateToken, authorizeAdmin, addExpenses);
-expenseRoutes.put("/:id", authenticateToken, authorizeAdmin, updatedExpense);
+expenseRoutes.put("/:id", authenticateToken, updatedExpense);
 expenseRoutes.delete("/:id", authenticateToken, authorizeAdmin, deleteExpense);
 
 module.exports = expenseRoutes;
